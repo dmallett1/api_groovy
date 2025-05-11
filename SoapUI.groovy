abc8a4506164a32e076674d28e73247bbc87aed6
Darren Mallett <dmallett1@users.noreply.github.com>
2025-05-11 12:54:47 +0100

    Create SoapUI.groovy

---
+++ SoapUI.groovy
@@ -0,0 +1,27 @@
+/**
+ * SoapUI class
+ * @version 1.0
+ */
+
+class SoapUI
+{
+	//instance variables
+	static context
+	static log
+    static String column
+    static String data
+    static def tblprefix
+    def runner
+    static def testRunner
+    static def testCase
+    static def testSuite
+    static def project
+    static def response
+    static def doc
+    static def exp_value
+    static def act_value
+    static def dir, dir2
+    static Integer uuidCount
+    static def length
+    static def parameter
+}
